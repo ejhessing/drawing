33da6a4d9586e48cb4f555f885afcdc68e4e1ce9
Erwin <ejhessing@gmail.com>
2016-05-10 18:56:01 +1200

    Added mouse effects to drawing App

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Create a drawing app
 
 var color = $('.selected').css("background-color");
-
-
+var $canvas = $("canvas");	
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
-$(".controls li").click(function(){
+$(".controls").on("click", "li" ,function(){
 	//Deselect sibling elements
 	$(this).siblings().removeClass("selected");
 	//select clicked elements
@@ -15,7 +17,7 @@ $(".controls li").click(function(){
 
 });
 
-//When "New Color Button" is clicked 
+//When "New Color" Button is clicked 
 $("#revealColorSelect").click(function(){
 	//Show or hide colorSelect 
 	changeColor();
@@ -37,9 +39,37 @@ $("input[type=range").change(changeColor);
 
 
 
-// When add color is clicked
+// When "Add Color" button is clicked
+$("#addNewColor").click(function(){
 	//append the color to the control ul
+	var $newColor = $("<li></li>");
+	$newColor.css("background-color", $('#newColor').css("background-color"));
+	$(".controls ul").append($newColor);
 	//select the new color
+	$newColor.click();
+});
 
-//on mouse events on the vancus 
-	//draw lines		
+
+
+//on mouse events on the canvas 
+$canvas.mousedown(function(e){
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e){
+	//draw lines
+	if(mouseDown){
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		context.lineTo(e.offsetX, e.offsetY);
+		context.strokeStyle = color;
+		context.stroke();
+		lastEvent = e;
+	}
+}).mouseup(function(){
+	mouseDown = false;
+})		
+
+
+
+
+
