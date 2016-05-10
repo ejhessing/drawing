df108ce94c59ddb888b2915536be0e8ebc9e498b
Erwin <ejhessing@gmail.com>
2016-05-10 17:32:57 +1200

    Done sudo code for app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// Create a drawing app
+
+//When clicking on control list items
+	//Deselect sibling elements
+	//select clicked elements
+
+
+//When "New Color Button" is clicked 
+	//Show or hide colorSelect 
+
+//When color sliders change 
+	//update the new color span
+
+// When add color is clicked
+	//append the color to the control ul
+	//select the new color
+
+//on mouse events on the vancus 
+	//draw lines		
