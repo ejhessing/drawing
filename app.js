7e3d3304ca20200dac83e6e2bc15c45dbeaa85c4
Erwin <ejhessing@gmail.com>
2016-05-10 17:55:11 +1200

    Added jQuery to color slider

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 // Create a drawing app
 
-//When clicking on control list items
-	//Deselect sibling elements
-	//select clicked elements
+var color = $('.selected').css("background-color");
 
+
+
+//When clicking on control list items
+$(".controls li").click(function(){
+	//Deselect sibling elements
+	$(this).siblings().removeClass("selected");
+	//select clicked elements
+	$(this).addClass("selected");
+	//cashe current color
+	color = $(this).css("background-color");
+
+});
 
 //When "New Color Button" is clicked 
+$("#revealColorSelect").click(function(){
 	//Show or hide colorSelect 
+	changeColor();
+	$("#colorSelect").toggle();
+
+});
+
+	//update the new color span
+function changeColor() {
+	var r = $("#red").val();
+	var g = $("#green").val();
+	var b = $("#blue").val();
+
+	$('#newColor').css("background-color","rgb(" + r + "," + g + "," + b + ")" );
+}
 
 //When color sliders change 
-	//update the new color span
+$("input[type=range").change(changeColor);
+
+
 
 // When add color is clicked
 	//append the color to the control ul
